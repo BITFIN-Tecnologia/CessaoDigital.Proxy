d1755406db42106e26ba4266fd2b5ded0b5fd5af
Israel Aece <israel.aece@bitfin.com.br>
2023-07-24 21:15:33 -0300

    Patch para alteração de status do fornecedor.

--- CessaoDigital.Proxy/Comunicacao/APIs/Instituicao.cs
+++ CessaoDigital.Proxy/Comunicacao/APIs/Instituicao.cs
@@ -1,18 +1,35 @@
 ﻿// Copyright (c) 2023 - BITFIN Software Ltda. Todos os Direitos Reservados.
 // Código exclusivo para consumo dos serviços (APIs) da Plataforma Cessão Digital.
 
+using CessaoDigital.Proxy.Utilitarios;
+using System.Text;
+
 namespace CessaoDigital.Proxy.Comunicacao.APIs
 {
     /// <summary>
-    /// 
+    /// Conjuto de API's para gestão pela Instituição Financeira através do produto Antecipação à Fornecedores.
     /// </summary>
     public class Instituicao : API
     {
         /// <summary>
-        /// 
+        /// Inicializa a API para Instituições Financeiras.
         /// </summary>
         /// /// <param name="proxy">Instância da classe <see cref="HttpClient"/> gerada pelo proxy.</param>
         public Instituicao(HttpClient proxy)
             : base(proxy) { }
+
+        /// <summary>
+        /// Permite à Instituição alternar o status do Fornecedor.
+        /// </summary>
+        /// <param name="status">Status do Fornecedor.</param>
+        /// <param name="cancellationToken">Instrução para eventual cancelamento da requisição.</param>
+        public async Task AtualizarFornecedor(DTOs.StatusDoFornecedor status, CancellationToken cancellationToken = default)
+        {
+            using (var requisicao = new HttpRequestMessage(HttpMethod.Patch, $"instituicao/fornecedores/{status.Codigo}/status")
+            {
+                Content = new StringContent(Serializador.Serializar(status), Encoding.UTF8, this.MimeType.MediaType)
+            })
+                await Executar(requisicao, cancellationToken);
+        }
     }
 }
---
+++ CessaoDigital.Proxy/DTOs/StatusDoFornecedor.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) 2023 - BITFIN Software Ltda. Todos os Direitos Reservados.
+// Código exclusivo para consumo dos serviços (APIs) da Plataforma Cessão Digital.
+
+using System.Diagnostics;
+
+namespace CessaoDigital.Proxy.DTOs
+{
+    /// <summary>
+    /// Status de formalização do Fornecedor.
+    /// </summary>
+    [DebuggerDisplay("Habilitado: {Habilitado}")]
+    public class StatusDoFornecedor
+    {
+        /// <summary>
+        /// Código que identifica o Fornecedor unificamente na Plataforma.
+        /// </summary>
+        public Guid Codigo { get; set; }
+
+        /// <summary>
+        /// Indica se o fornecedor está ou não habilitado.
+        /// </summary>
+        public bool Habilitado { get; set; }
+
+        /// <summary>
+        /// Mensagem customizada para justificar o status.
+        /// </summary>
+        public string Mensagem { get; set; }
+    }
+}
