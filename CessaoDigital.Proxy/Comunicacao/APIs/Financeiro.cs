72f0b04292c6f687175c1e78aaecfff9400f51df
Israel Aece <israel.aece@bitfin.com.br>
2023-07-26 11:00:20 -0300

    API Financeira e novos DTOs.

--- CessaoDigital.Proxy/Comunicacao/APIs/Financeiro.cs
+++ CessaoDigital.Proxy/Comunicacao/APIs/Financeiro.cs
@@ -1,18 +1,36 @@
 ﻿// Copyright (c) 2023 - BITFIN Software Ltda. Todos os Direitos Reservados.
 // Código exclusivo para consumo dos serviços (API's) da Plataforma Cessão Digital.
 
+using CessaoDigital.Proxy.Utilitarios;
+
 namespace CessaoDigital.Proxy.Comunicacao.APIs
 {
     /// <summary>
-    /// 
+    /// Informações financeiras sobre a contratação do serviço.
     /// </summary>
     public class Financeiro : API
     {
         /// <summary>
-        /// 
+        /// Inicializa a API de informações financeiras.
         /// </summary>
         /// <param name="proxy">Instância da classe <see cref="HttpClient"/> gerada pelo proxy.</param>
         public Financeiro(HttpClient proxy)
             : base(proxy) { }
+
+        /// <summary>
+        /// Relação de Fechamentos.
+        /// </summary>
+        /// <param name="cancellationToken">Instrução para eventual cancelamento da requisição.</param>
+        /// <returns>Relação de Fechamentos realizados.</returns>
+        /// <remarks>Fechamentos financeiros que resumem o consumo mensal do contratante.</remarks>
+        /// <exception cref="ErroNaRequisicao">Exceção disparada se alguma falha ocorrer durante a requisição ou em seu processamento.</exception>
+        public async Task<IEnumerable<DTOs.Fechamento>> Fechamentos(CancellationToken cancellationToken = default)
+        {
+            using (var requisicao = new HttpRequestMessage(HttpMethod.Get, "financeiro/fechamentos"))
+                return await Executar(
+                    requisicao,
+                    async resposta => await resposta.Content.ReadAs<IEnumerable<DTOs.Fechamento>>(cancellationToken),
+                    cancellationToken);
+        }
     }
 }
---
+++ CessaoDigital.Proxy/DTOs/Fechamento.cs
@@ -0,0 +1,64 @@
+﻿// Copyright (c) 2023 - BITFIN Software Ltda. Todos os Direitos Reservados.
+// Código exclusivo para consumo dos serviços (API's) da Plataforma Cessão Digital.
+
+using System.Diagnostics;
+
+namespace CessaoDigital.Proxy.DTOs
+{
+    /// <summary>
+    /// Fechamento financeiro.
+    /// </summary>
+    [DebuggerDisplay("{Mes}/{Ano} - {Valor} - {Contratante,nq}")]
+    public class Fechamento
+    {
+        /// <summary>
+        /// Entidade contratante.
+        /// </summary>
+        public Contratante Contratante { get; set; }
+
+        /// <summary>
+        /// Data de apuração.
+        /// </summary>
+        public DateTime Data { get; set; }
+
+        /// <summary>
+        /// Ano do período de apuração.
+        /// </summary>
+        public int Ano { get; set; }
+
+        /// <summary>
+        /// Mês do período de apuração.
+        /// </summary>
+        public int Mes { get; set; }
+
+        /// <summary>
+        /// Quantidade de recebíveis registrados no período.
+        /// </summary>
+        public int Quantidade { get; set; }
+
+        /// <summary>
+        /// Valor unitário por registro da faixa atingida.
+        /// </summary>
+        public decimal ValorUnitario { get; set; }
+
+        /// <summary>
+        /// Valor total apurado.
+        /// </summary>
+        public decimal ValorTotal { get; set; }
+
+        /// <summary>
+        /// Data para pagamento da fatura.
+        /// </summary>
+        public DateTime DataDeVencimento { get; set; }
+
+        /// <summary>
+        /// Status de pagamento.
+        /// </summary>
+        public string Status { get; set; }
+
+        /// <summary>
+        /// Data do status de pagamento.
+        /// </summary>
+        public DateTime DataDoStatus { get; set; }
+    }
+}
